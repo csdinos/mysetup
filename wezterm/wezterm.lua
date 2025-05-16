0ab90db86a1c8b6e28a17222cfc5b8b518394f8d
csdinos <dinos.vlachantonis@gmail.com>
2025-05-16 18:45:09 +0200

    wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require "wezterm"
+local config = {}
+local forceDark = 1
+
+function scheme_for_appearance(appearance)
+  if forceDark == 1 or appearance:find "Dark" then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+config.background = {
+  {
+    source = {
+     -- File = '~/Pictures/katana-wallhaven.jpg',
+	File = wezterm.home_dir .. '/Pictures/katana-wallhaven.jpg',
+    },
+    hsb = {
+      brightness = 0.1,
+      hue = 1.0,
+      saturation = 1.0,
+    },
+  },
+}
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+return config
